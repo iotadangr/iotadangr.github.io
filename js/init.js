c8275b9f85e6f78cbfc04493fdbb7c73db938e49
iotadangr <82296611+iotadangr@users.noreply.github.com>
2021-04-10 20:49:04 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+    apiKey: "AIzaSyAZkuLSymPF399BixU-0IyWs4cXMQIB5F8",
+    authDomain: "iotadanagr.firebaseapp.com",
+    projectId: "iotadanagr",
+    storageBucket: "iotadanagr.appspot.com",
+    messagingSenderId: "899636666263",
+    appId: "1:899636666263:web:f7e50c0987b80b60282ed2"
+
 });
