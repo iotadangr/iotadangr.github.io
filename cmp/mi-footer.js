c0d4bc7930bb0ec5fd2a68069b6aad425192dcd9
iotadangr <82296611+iotadangr@users.noreply.github.com>
2021-04-11 00:18:46 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -3,7 +3,7 @@ class MiFooter
   connectedCallback() {
     this.innerHTML = /* html */
       `&copy; 2021
-      Gilberto Pacheco Gallegos.`;
+      Adan Gomez Reyes.`;
   }
 }
 customElements.define(
